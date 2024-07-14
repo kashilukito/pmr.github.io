e0bbea6787d4f0c49941468e3c3bed1d31c98029
kashilukito <budegunter@gmail.com>
2024-07-14 13:55:11 +0700

    pmr
    pmr

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>web Clan P.m.r</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-dark" style="background:#000">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#"><img src="e5fb36b7-53ba-4ee1-adf3-4f07ee5bb056.jpg" width="80px" alt=""></a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">[sedang tahap pengembangan :(]</a>
+    </li>
+  
+</nav>
+
+<div class="container-fluid">
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>apa itu PMR?</p>
+
+<blockquote cite="http://www.worldwildlife.org/who/index.html">
+PMR adalah sebuah clan dari game yang bernama
+ Simple sandbox2 dan simple sandbox 3, PMR di buat untuk menciptakan organisasi simple sandbox 2 dan 3
+</blockquote>
+
+</body>
+</html>
+
+
+</div>
+
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 90%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<h2>LIST ANGGOTA</h2>
+
+<table>
+  <tr>
+    <th>Anggota</th>
+    <th>Pangkat</th>
+    <th>Negara</th>
+  </tr>
+  <tr>
+    <td>shiro</td>
+    <td>Leader 1</td>
+    <td>indonesia</td>
+  </tr>
+  <tr>
+    <td>Finley</td>
+    <td>Leader 2</td>
+    <td>indonesia</td>
+  </tr>
+  <tr>
+    <td>Evos</td>
+    <td>wakil</td>
+    <td>indonesia</td>
+  </tr>
+  <tr>
+    <td>Frost</td>
+    <td>Lol Rank</td>
+    <td>indonesia</td>
+  </tr>
+  <tr>
+    <td>vaggie</td>
+    <td>bronze</td>
+    <td>indonesia</td>
+  </tr>
+  <tr>
+    <td>zaf indo</td>
+    <td>bronze</td>
+    <td>indonesia</td>
+  </tr>
+  <tr>
+     <td>VZ</td>
+     <td>bronze</td>
+     <td>indonesia</td>
+</tr>
+<tr>
+</table>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Pro PVP</h2>
+
+<ul>
+  <li>shiro</li>
+  <li>Finley</li>
+  <li>slot kosong</li>
+  <li>slot kosong</li>
+  <li>slot kosong</li>
+  <li>slot kosong</li>
+  <li>slot kosong</li>
+</ul>  
+
+<h1>BANTU GUA DENGAN SUBREK CHANNEL VERMUX_SHIRO</h1>
+<h2>https://www.youtube.com/@Kashilukito</h2>
